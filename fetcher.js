0465a0945db3466124b92cf92ff34675c5af211f
John Chia <john.chia0106@gmail.com>
2022-01-19 18:49:14 -0800

    odified the file to include the edge cases

--- fetcher.js
+++ fetcher.js
@@ -2,26 +2,46 @@ const args = process.argv;
 let newArgs = args.splice(2);
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-
-const fetcher = (url, path) => {
-  let content = '';
-  
-  request(url, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    content = body;
-    const size = content.length;
-    fs.writeFile(path, content, { flag: 'a+' }, err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Downloaded and saved ${size} bytes to ${path}`);
-    });
+const writeFile = (path, body) => {
+  console.log('writing file', path);
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    process.exit();
   });
 };
 
+const fetcher = (url, path) => {
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error.message);
+      return process.exit();
+    }
+
+    if (fs.existsSync(path)) {
+      rl.question('File exits, overwrite? Y/N', (key) => {
+        if (key !== 'Y') {
+          return process.exit();
+        }
+        console.log('YES');
+        writeFile(path, body);
+      });
+      return;
+    }
+
+    writeFile(path, body);
+  });
+};
+
+
 fetcher(newArgs[0], newArgs[1]);
-
-
